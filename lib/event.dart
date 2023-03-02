8788957c24c59cb107686954138716988331e275
Amine Alami <alamia502@gmail.com>
2023-03-02 09:59:09 +0100

    Fetching data successful

--- lib/event.dart
+++ lib/event.dart
@@ -2,49 +2,46 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Event {
-  String title; //events.pages.titles.display
-  String imageUrl; //events.pages.originalimage.source
-  String year; //events.year
-  String description; //event.description
+  var id;
+  var title;
+  var imageUrl;
+  var year;
+  var description;
 
-  Event({
-    required this.title,
-    required this.imageUrl,
-    required this.year,
-    required this.description,
-  });
+  Event();
 
-   fromJson(json): Post {
-    Post p = new Post()
-    p.name = ...
-    return p
+  factory Event.fromJson(json) {
+    Event event = Event();
+    event.title = json["pages"][0]["titles"]["normalized"];
+    event.imageUrl = json["pages"][0]["originalimage"] != null
+        ? json["pages"][0]["originalimage"]["source"]
+        : "no image to display";
+    event.description = json["text"];
+    event.year = json["year"];
+    return event;
   }
 
-
+  @override
+  String toString() {
+    return "\ntitle = $title"
+        "\nimageUrl = $imageUrl"
+        "\ndescription = $description"
+        "\nyear = $year";
+  }
 }
 
+Future<List<Event>> fetch() async {
+  var url = Uri.parse(
+      'https://api.wikimedia.org/feed/v1/wikipedia/fr/onthisday/events/03/01');
+  var response = await http.get(url);
 
-  Future<Event> fetch() async {
-    var url = Uri.parse(
-        'https://api.wikimedia.org/feed/v1/wikipedia/fr/onthisday/events/03/01');
-    var response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      Iterable l = json.decode(response.body);
-List<Event> events = List<Event>.from(l.map((model)=> Event.fromJson(model)));
-
-
-      var jsonResponse = jsonDecode(response.body.map();
-      return Event(
-        title: jsonResponse['events.pages.titles.display'],
-        imageUrl: jsonResponse['imageUrl'],
-        year: jsonResponse['date'],
-        description: jsonResponse['description'],
-      );
-    } else {
-      throw Exception('Failed to load data');
-    }
+  if (response.statusCode == 200) {
+    Map<String, dynamic> jsonData = json.decode(response.body);
+    List<dynamic> eventsJson = jsonData['events'];
+    List<Event> events =
+        eventsJson.map((eventJson) => Event.fromJson(eventJson)).toList();
+    return events;
+  } else {
+    throw Exception('Failed to load data');
   }
-void main(List<String> args) {
-  print(Event.fetch());
 }
